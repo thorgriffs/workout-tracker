70bca1946ae895d64120d32362f565a091fde486
thorgriffs <22.kelliking@gmail.com>
2021-02-17 17:15:53 -0500

    setting up server.js

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Exporting an object containing all of our models
+
+/* Not sure what belongs here yet
+module.exports = {
+    Something: require("./Something"),
+    Somethingelse: require("./Somethingelse")
+  }; */
