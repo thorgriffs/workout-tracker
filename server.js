7c1264beae93b13b97d332fce5f9de15ebea6ba5
thorgriffs <22.kelliking@gmail.com>
2021-02-22 12:16:35 -0500

    added html routes to server.js, moved all .js files to public/js, moved style.css to public/css, updated file paths in html files

--- server.js
+++ server.js
@@ -17,8 +17,19 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-
 // using the week 17 activities example... not sure if get/post routes belong here for the homework
+
+// Routes
+// route to stats.html
+app.get("/stats", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/stats.html"));
+});
+
+// route to exercise.html
+app.get("/exercise", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/exercise.html"));
+});
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
