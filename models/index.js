301c171b254c54db8110ca59169e38541dba71ae
thorgriffs <22.kelliking@gmail.com>
2021-02-22 17:27:46 -0500

    adding workout schema, adding routes on server.js to the stats page, added path for html paths to work

--- models/index.js
+++ models/index.js
@@ -1,7 +1,5 @@
 // Exporting an object containing all of our models
 
-/* Not sure what belongs here yet
 module.exports = {
-    Something: require("./Something"),
-    Somethingelse: require("./Somethingelse")
-  }; */
+    Workout: require("./Workout")
+  }; 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,7 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 // using the week 17 activities example... not sure if get/post routes belong here for the homework
 
-// Routes
+// HTML Routes
 // route to stats.html
 app.get("/stats", function(req, res) {
   res.sendFile(path.join(__dirname, "public/stats.html"));
@@ -30,6 +31,26 @@ app.get("/exercise", function(req, res) {
   res.sendFile(path.join(__dirname, "public/exercise.html"));
 });
 
+//API Routes
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({}, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({}, (err, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
